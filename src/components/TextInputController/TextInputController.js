8778feeeda9c790d2cc0d90570b1dfb33bb7ea87
TelamonPegasus <telamon025115@gmail.com>
2022-06-17 14:26:22 +0100

    create TextInputController

---
+++ src/components/TextInputController/TextInputController.js
@@ -0,0 +1,39 @@
+import { Controller } from "react-hook-form";
+import { Typography, TextField } from "@mui/material";
+
+const stylesError = { color: "crimson", paddingTop: 4 };
+
+const TextInputController = ({
+  control,
+  name,
+  label,
+  defaultValue,
+  error,
+  message,
+}) => (
+  <>
+    <Controller
+      name={name}
+      control={control}
+      rules={{ required: true }}
+      defaultValue={defaultValue}
+      render={({ field: { ref, ...field } }) => (
+        <TextField
+          {...field}
+          inputRef={ref}
+          variant="filled"
+          label={label}
+          error={error}
+          fullWidth
+          margin="dense"
+        />
+      )}
+    />
+
+    <Typography variant="inherit" style={stylesError}>
+      {message}
+    </Typography>
+  </>
+);
+
+export default TextInputController;
