9a63311fea79f1f683a200e861782b0fbb24e21d
TelamonPegasus <telamon025115@gmail.com>
2022-06-17 18:45:13 +0100

    edit server file

--- server.js
+++ server.js
@@ -6,8 +6,15 @@ const middlewares = jsonServer.defaults({ static: "./build" });
 const PORT = process.env.PORT || 5000;
 
 server.use(middlewares);
+
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
+
 server.use(router);
 
 server.listen(PORT, () => {
-  console.log("server is running");
+  console.log(`Server is running on ${PORT}`);
 });
