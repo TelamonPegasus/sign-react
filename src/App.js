e17b9dbbd35f19d5c04b28aee5bbf303713151b9
TelamonPegasus <telamon025115@gmail.com>
2022-06-17 14:24:32 +0100

    add tab list and tab panel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+import Box from "@mui/material/Box";
+
+import Tab from "@material-ui/core/Tab";
+import { TabContext } from "@material-ui/lab";
+import TabList from "@material-ui/lab/TabList";
+import TabPanel from "@material-ui/lab/TabPanel";
+
+import { SignInContainer } from "./pages/SignInContainer";
+import { SignUpContainer } from "./pages/SignUpContainer";
+
+import { RegistrationProvider } from "context/RegistrationContext";
+
+export default function App() {
+  const [value, setValue] = useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ width: "100%", typography: "body1" }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <TabList
+            onChange={handleChange}
+            aria-label="lab API tabs example"
+            centered
+          >
+            <Tab label="Sign in" value="1" />
+            <Tab label="Sign up" value="2" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">
+          {/* <RegistrationProvider> */}
+          <SignInContainer setValue={setValue} />
+          {/* </RegistrationProvider> */}
+        </TabPanel>
+        <TabPanel value="2">
+          <RegistrationProvider>
+            <SignUpContainer setValue={setValue} />
+          </RegistrationProvider>
+        </TabPanel>
+      </TabContext>
+    </Box>
   );
 }
-
-export default App;
