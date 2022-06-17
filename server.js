401b9673e2f203cbb3cdd56abffe0eda450325bb
TelamonPegasus <telamon025115@gmail.com>
2022-06-17 18:16:15 +0100

    server.js create

---
+++ server.js
@@ -0,0 +1,13 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("data/db.json");
+const middlewares = jsonServer.defaults({ static: "./build" });
+
+const PORT = process.env.PORT || 5000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log("server is running");
+});
