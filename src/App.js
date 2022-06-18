5892db41700877e8d3f56b6142c0de9804421e24
TelamonPegasus <telamon025115@gmail.com>
2022-06-18 13:12:51 +0100

    remove value and setValue with handleChange, remove props, import RegistrationContext and pass props forward

--- src/App.js
+++ src/App.js
@@ -1,48 +1,51 @@
-import { useState } from "react";
+import { useContext } from "react";
+import { Box } from "@mui/material";
+import { Tab } from "@material-ui/core";
+import { TabList, TabPanel, TabContext } from "@material-ui/lab";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import Box from "@mui/material/Box";
-
-import Tab from "@material-ui/core/Tab";
-import { TabContext } from "@material-ui/lab";
-import TabList from "@material-ui/lab/TabList";
-import TabPanel from "@material-ui/lab/TabPanel";
-
+import { RegistrationContext } from "./context/RegistrationContext";
 import { SignInContainer } from "./pages/SignInContainer";
 import { SignUpContainer } from "./pages/SignUpContainer";
 
-import { RegistrationProvider } from "context/RegistrationContext";
-
 export default function App() {
-  const [value, setValue] = useState("1");
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  const { tabValue, handleChangeTabValue } = useContext(RegistrationContext);
 
   return (
-    <Box sx={{ width: "100%", typography: "body1" }}>
-      <TabContext value={value}>
-        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-          <TabList
-            onChange={handleChange}
-            aria-label="lab API tabs example"
-            centered
-          >
-            <Tab label="Sign in" value="1" />
-            <Tab label="Sign up" value="2" />
-          </TabList>
-        </Box>
-        <TabPanel value="1">
-          {/* <RegistrationProvider> */}
-          <SignInContainer setValue={setValue} />
-          {/* </RegistrationProvider> */}
-        </TabPanel>
-        <TabPanel value="2">
-          <RegistrationProvider>
-            <SignUpContainer setValue={setValue} />
-          </RegistrationProvider>
-        </TabPanel>
-      </TabContext>
-    </Box>
+    <>
+      <Box sx={{ width: "100%", typography: "body1" }}>
+        <TabContext value={tabValue}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <TabList
+              onChange={handleChangeTabValue}
+              aria-label="lab API tabs example"
+              centered
+            >
+              <Tab label="Sign in" value="1" />
+              <Tab label="Sign up" value="2" />
+            </TabList>
+          </Box>
+          <TabPanel value="1">
+            <SignInContainer />
+          </TabPanel>
+          <TabPanel value="2">
+            <SignUpContainer />
+          </TabPanel>
+        </TabContext>
+      </Box>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
   );
 }
