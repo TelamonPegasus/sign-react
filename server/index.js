dcf097518cd68f588ace123158ff34620b9e2826
TelamonPegasus <telamon025115@gmail.com>
2022-06-19 15:42:32 +0100

    edit server, add if statement, rename server to app

--- server/index.js
+++ server/index.js
@@ -4,16 +4,24 @@ const express = require("express");
 const jsonServer = require("json-server");
 const cors = require("cors");
 
-const server = express();
+const app = express();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 
-server.use(cors());
-server.use(bodyParser.json());
-server.use("/", router);
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/", router);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
 
