2f081e8866a8f3649af90bbcf85fc1997778ec9f
TelamonPegasus <telamon025115@gmail.com>
2022-06-21 10:57:58 +0100

    comment if production

--- server.js
+++ server.js
@@ -60,13 +60,17 @@ const routes = require("./routes")(app, fs);
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/build")));
+// if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"), (err) => {
+    if (err) {
+      res.status(500).send(err);
+    }
   });
-}
+});
+// }
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
