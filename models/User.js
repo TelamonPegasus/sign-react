4125bc3ea8a720d5f8b7dc61dafd15d854a9b68f
TelamonPegasus <telamon025115@gmail.com>
2022-06-25 00:11:25 +0100

    add role to schema

--- models/User.js
+++ models/User.js
@@ -18,12 +18,21 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  role: {
+    User: {
+      type: Number,
+      default: 2001,
+    },
+    Editor: Number,
+    Admin: Number,
+  },
+  refreshToken: String,
   date: {
     type: Date,
     default: Date.now,
   },
 });
 
-const User = mongoose.model("registeredusers", userSchema);
+const User = mongoose.model("registeredusers", userSchema); // change to singular
 
 module.exports = User;
