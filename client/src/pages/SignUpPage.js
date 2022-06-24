a77473dae71318c45b42b0b0126fd6599f864655
TelamonPegasus <telamon025115@gmail.com>
2022-06-25 00:08:44 +0100

    resign from context and add registerUser function based on server response

--- client/src/pages/SignUpPage.js
+++ client/src/pages/SignUpPage.js
@@ -1,12 +1,27 @@
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router";
 
 import { validationForm } from "validationForm";
 import { useRegistrationContext } from "context/RegistrationContext";
 import { SignUp } from "components/LogRegisterForm";
 
+import api from "api";
+
+const toastConfig = {
+  position: "top-center",
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+};
+
 const SignUpPage = ({ setValue }) => {
-  const { users, addUser, isLoading } = useRegistrationContext();
+  const REGISTER_ENDPOINT = "/register";
+  const { users, isLoading } = useRegistrationContext();
   const {
     handleSubmit,
     control,
@@ -16,9 +31,24 @@ const SignUpPage = ({ setValue }) => {
     resolver: yupResolver(validationForm),
   });
 
-  const sendData = (data) => {
-    addUser(data);
-  };
+  const navigate = useNavigate();
+
+  const sendData = (data) => registerUser(data);
+
+  async function registerUser(newUser) {
+    try {
+      const response = await api.post(`${REGISTER_ENDPOINT}`, newUser);
+
+      toast.success(response.message, toastConfig);
+      navigate("/login");
+    } catch (error) {
+      toast.error(error?.response?.data?.message, toastConfig);
+
+      if (error?.response.status === 409) {
+        navigate("/login");
+      }
+    }
+  }
 
   return (
     <SignUp
