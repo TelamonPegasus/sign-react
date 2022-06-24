5f7fb84bc92e9e81d9d23ee59de81f05f62a752c
TelamonPegasus <telamon025115@gmail.com>
2022-06-25 00:12:00 +0100

    create routers

---
+++ routes/auth.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const authController = require("../controllers/authController");
+
+router.post("/", authController.handleLogin);
+
+module.exports = router;
---
+++ routes/root.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+router.get("*", (_, response) => {
+  response.sendFile(path.join(__dirname, "client/build/index.html"), (err) => {
+    if (err) {
+      response.status(500).send(err);
+    }
+  });
+});
+
+module.exports = router;
