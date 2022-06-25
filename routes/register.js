255a050a3de46f3495f42b6c6e100bad849878df
TelamonPegasus <telamon025115@gmail.com>
2022-06-25 16:09:06 +0100

    change order imports

--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,6 @@
+const registerController = require("../controllers/registerController");
 const express = require("express");
 const router = express.Router();
-const registerController = require("../controllers/registerController");
 
 router.post("/", registerController.handleRegisterNewUser);
 
