d3cfca9a3b7182a93cd01cb219de4ed7048e3295
TelamonPegasus <telamon025115@gmail.com>
2022-06-25 17:31:59 +0100

    import path

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
