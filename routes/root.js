d82e6ff7529b2d680699ba30de642ad8dc78971d
TelamonPegasus <telamon025115@gmail.com>
2022-06-25 08:40:15 +0100

    comment file

--- routes/root.js
+++ routes/root.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const router = express.Router();
-const path = require("path");
+// const express = require("express");
+// const router = express.Router();
+// const path = require("path");
 
-router.get("/", (_, response) => {
-  response.sendFile(path.join(__dirname, "client/build/index.html"), (err) => {
-    if (err) {
-      response.status(500).send(err);
-    }
-  });
-});
+// router.get("/", (_, response) => {
+//   response.sendFile(path.join(__dirname, "client/build/index.html"), (err) => {
+//     if (err) {
+//       response.status(500).send(err);
+//     }
+//   });
+// });
 
-module.exports = router;
+// module.exports = router;
