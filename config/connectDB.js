77e57e4b27e96895d095bf1fc04ad148d9cc7228
TelamonPegasus <telamon025115@gmail.com>
2022-06-25 10:47:19 +0100

    rename variable

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_ACCESS, {
+    await mongoose.connect(process.env.MONGODB_ACCESS, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
