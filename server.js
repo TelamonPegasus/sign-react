c4e2734ab8b8edcf5487dc75543a936b9d293b3e
TelamonPegasus <telamon025115@gmail.com>
2022-06-25 10:57:35 +0100

    change urlencoded extended to true

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ connectDB();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // routes
 
