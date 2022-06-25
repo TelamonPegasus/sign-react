669d1b361cd6d69fe0933701cdcdaa8f59c7f2ab
TelamonPegasus <telamon025115@gmail.com>
2022-06-25 20:09:21 +0100

    edit page, add icons

--- client/src/pages/NotFoundPage.js
+++ client/src/pages/NotFoundPage.js
@@ -1,15 +1,27 @@
+import { Link } from "react-router-dom";
+import { VscSearchStop } from "react-icons/vsc";
+import { AiTwotoneHome } from "react-icons/ai";
+
 const styles = {
-  container: { marginTop: 70, padding: "0 20px 0 20px" },
+  container: { margin: 70, padding: "0 20px 0 20px", textAlign: "center" },
+  icon: { fontSize: 40 },
   heading: { color: "#d63e2f" },
 };
 
 const NotFoundPage = () => (
   <div style={styles.container}>
+    <p>
+      <VscSearchStop style={styles.icon} />
+    </p>
     <h1>
       Page <span style={styles.heading}>Not Found</span>
     </h1>
 
-    <p>Please check your url address.</p>
+    <p>Please check your url address or</p>
+
+    <Link to="/">
+      <AiTwotoneHome style={{ ...styles.icon, ...styles.heading }} />
+    </Link>
   </div>
 );
 
