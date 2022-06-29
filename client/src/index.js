031a64ce5ad9749d6ff9ec510f8d419974b59103
TelamonPegasus <telamon025115@gmail.com>
2022-06-29 12:35:30 +0100

    import AuthProvider, add router and Routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
-import { RegistrationProvider } from "./context/RegistrationContext";
+import { AuthProvider } from "context/AuthProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RegistrationProvider>
-      <App />
-    </RegistrationProvider>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
   </React.StrictMode>
 );
