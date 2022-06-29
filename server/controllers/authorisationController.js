eec91b4ca62db492184407ab5dd211286a3fad40
TelamonPegasus <telamon025115@gmail.com>
2022-06-29 12:34:31 +0100

    edit refreshToken expires

--- server/controllers/authorisationController.js
+++ server/controllers/authorisationController.js
@@ -38,13 +38,13 @@ const handleLogin = async (request, response) => {
         },
       },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "5s" }
+      { expiresIn: "5s" } // change it
     );
 
     const refreshToken = jwt.sign(
       { email: foundUser.email },
       process.env.REFRESH_TOKEN_SECRET,
-      { expiresIn: "1d" }
+      { expiresIn: "1h" } // change it
     );
 
     // Saving refreshToken with current user
@@ -53,6 +53,8 @@ const handleLogin = async (request, response) => {
     );
 
     const currentUser = { ...foundUser, refreshToken };
+
+    console.log("current", currentUser);
 
     usersDB.setUsers([...otherUsers, currentUser]);
 
@@ -68,45 +70,11 @@ const handleLogin = async (request, response) => {
       maxAge: 24 * 60 * 60 * 1000,
     });
 
-    response.json({ accessToken });
+    console.log("accessToken", accessToken);
+    response.json({ accessToken, roles });
   } else {
     response.sendStatus(401);
   }
 };
 
 module.exports = { handleLogin };
-
-// const User = require("../models/User");
-// const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
-
-// const handleLogin = async (request, response) => {
-//   const { email, password } = request.body;
-
-//   if (email === "" || password === "") {
-//     response.status(400).json("All fields are required!");
-//   }
-
-//   try {
-//     let user;
-
-//     try {
-//       user = await User.findOne({ email });
-
-//       const validatedUser = await bcrypt.compare(password, user.password);
-
-//       !validatedUser &&
-//         response.status(403).json("Wrong pasword. Please type again!");
-
-//       const token = jwt.sign({ name: user.name }, "verySecretValue", {
-//         expiresIn: "5s",
-//       });
-
-//       response.status(200).json({ message: "You are logged in!", token });
-//     } catch (error) {
-//       response.status(404).json("User Not Found!");
-//     }
-//   } catch (error) {
-//     response.status(500).json("Something Else Went Wrong!");
-//   }
-// };
