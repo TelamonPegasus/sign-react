0a6d6de307ec9d8ba6f28e18189ea2d6cb4a5cc3
TelamonPegasus <telamon025115@gmail.com>
2022-06-29 12:32:25 +0100

    transfer User to the renamed dir

---
+++ server/model/User.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    User: {
+      type: Number,
+      default: 2001,
+    },
+    Editor: Number,
+    Admin: Number,
+  },
+  refreshToken: String,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("registeredusers", userSchema); // change to singular
+
+module.exports = User;
