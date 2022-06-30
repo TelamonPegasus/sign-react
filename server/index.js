5f9c8c0c6f2ee32b130efa2d2d8fa3d579431515
TelamonPegasus <telamon025115@gmail.com>
2022-06-30 21:27:29 +0100

    heroku deploy bug

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 
-  router.get("*", (_, response) => {
+  router.get("/*", (_, response) => {
     response.sendFile(
       path.join(__dirname, "../client/build/index.html"),
       (err) => {
