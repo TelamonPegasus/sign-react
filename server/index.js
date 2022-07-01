fda5ae3c556ee63ce0490ed4f553f3983676eede
TelamonPegasus <telamon025115@gmail.com>
2022-07-01 10:01:51 +0100

    add path to the root inside of the app.use

--- server/index.js
+++ server/index.js
@@ -55,7 +55,7 @@ const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 
-  app.use(rootRoute);
+  app.use("*", rootRoute);
 }
 
 app.use(errorHandler);
