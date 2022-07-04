52ae7ff9af8d5af48017fe47353f22780e9a305f
TelamonPegasus <telamon025115@gmail.com>
2022-07-04 20:58:13 +0100

    create UserDataPage, rename AdminPage to UsersPage

---
+++ client/src/pages/UserDataPage.js
@@ -0,0 +1,31 @@
+const styles = {
+  container: { marginTop: 70, padding: "0 20px 0 20px" },
+  heading: { color: "#d63e2f" },
+};
+
+const UserDataPage = () => {
+  return (
+    <div style={styles.container}>
+      <h1>
+        Data <span style={styles.heading}>Available</span>
+      </h1>
+
+      <p>
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It
+        has roots in a piece of classical Latin literature from 45 BC, making it
+        over 2000 years old. Richard McClintock, a Latin professor at
+        Hampden-Sydney College in Virginia, looked up one of the more obscure
+        Latin words, consectetur, from a Lorem Ipsum passage, and going through
+        the cites of the word in classical literature, discovered the
+        undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33
+        of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by
+        Cicero, written in 45 BC. This book is a treatise on the theory of
+        ethics, very popular during the Renaissance. The first line of Lorem
+        Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
+        1.10.32.
+      </p>
+    </div>
+  );
+};
+
+export default UserDataPage;
