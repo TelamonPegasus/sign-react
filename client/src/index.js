9bb46df1b92c1d0b0175583ce7322f1ad708661c
TelamonPegasus <telamon025115@gmail.com>
2022-07-04 19:57:49 +0100

    order imports

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import "./index.css";
 import App from "./App";
-
 import { AuthProvider } from "context/AuthProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
