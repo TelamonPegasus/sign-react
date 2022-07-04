225b0bc2c88b669f332b93055a3a4feb8e731ba3
TelamonPegasus <telamon025115@gmail.com>
2022-07-04 21:04:47 +0100

    edit imports

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,9 +1,10 @@
 export { default as HomePage } from "./HomePage";
 export { default as CoursesPage } from "./CoursesPage";
 export { default as AboutPage } from "./AboutPage";
-export { default as UserContentPage } from "./UserContentPage";
+export { default as SecuredContentPage } from "./SecuredContentPage";
 export { default as SignInPage } from "./SignInPage";
 export { default as SignUpPage } from "./SignUpPage";
 export { default as NotFoundPage } from "./NotFoundPage";
 export { default as SecuredPageInfo } from "./SecuredPageInfo";
-export { default as AdminPage } from "./AdminPage";
+export { default as UsersPage } from "./UsersPage";
+export { default as UserDataPage } from "./UserDataPage";
