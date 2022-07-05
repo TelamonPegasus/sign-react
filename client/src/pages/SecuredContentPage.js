ffbd612765d5ee0f1a5ebb73bdca6f06a5f94939
TelamonPegasus <telamon025115@gmail.com>
2022-07-05 12:31:25 +0100

    add SubNavigation

--- client/src/pages/SecuredContentPage.js
+++ client/src/pages/SecuredContentPage.js
@@ -1,7 +1,7 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import { useAuthContext } from "context/AuthProvider";
-import { StyledButton } from "components/StyledButton";
+import { SubNavigation } from "components/Navigation";
 
 const styles = {
   container: { marginTop: 70, padding: "0 20px 0 20px" },
@@ -9,43 +9,32 @@ const styles = {
 };
 
 const SecuredContentPage = ({ allowedRoles }) => {
-  const navigate = useNavigate();
   const { auth } = useAuthContext();
 
-  const handleNavigateCourses = () => navigate("/secure-content/courses");
-  const handleNavigateData = () => navigate("/secure-content/data");
-  const handleNavigateUsers = () => navigate("/secure-content/users");
-
   return (
-    <div style={styles.container}>
-      <h1>
-        Welcome <span style={styles.heading}>{auth?.name}</span>
-      </h1>
+    <>
+      <div style={styles.container}>
+        <SubNavigation allowedRoles={allowedRoles} />
 
-      <StyledButton onClick={handleNavigateCourses} text="courses" />
+        <h1>
+          Welcome <span style={styles.heading}>{auth?.name}</span>
+        </h1>
 
-      <StyledButton onClick={handleNavigateData} text="data" />
-
-      {auth?.roles.find((role) => allowedRoles.includes(role)) && (
-        <StyledButton onClick={handleNavigateUsers} text="users" />
-      )}
-
-      <p>
-        There are many variations of passages of Lorem Ipsum available, but the
-        majority have suffered alteration in some form, by injected humour, or
-        randomised words which don't look even slightly believable. If you are
-        going to use a passage of Lorem Ipsum, you need to be sure there isn't
-        anything embarrassing hidden in the middle of text. All the Lorem Ipsum
-        generators on the Internet tend to repeat predefined chunks as
-        necessary, making this the first true generator on the Internet. It uses
-        a dictionary of over 200 Latin words, combined with a handful of model
-        sentence structures, to generate Lorem Ipsum which looks reasonable. The
-        generated Lorem Ipsum is therefore always free from repetition, injected
-        humour, or non-characteristic words etc.
-      </p>
-
-      <Outlet />
-    </div>
+        <p>
+          There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don't look even slightly believable.
+          If you are going to use a passage of Lorem Ipsum, you need to be sure
+          there isn't anything embarrassing hidden in the middle of text. All
+          the Lorem Ipsum generators on the Internet tend to repeat predefined
+          chunks as necessary, making this the first true generator on the
+          Internet. It uses a dictionary of over 200 Latin words, combined with
+          a handful of model sentence structures, to generate Lorem Ipsum which
+          looks reasonable. The generated Lorem Ipsum is therefore always free
+          from repetition, injected humour, or non-characteristic words etc.
+        </p>
+      </div>
+    </>
   );
 };
 
