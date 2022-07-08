5489b5167c1fb514fe476c0b0f393eb18f1d13e2
TelamonPegasus <telamon025115@gmail.com>
2022-07-08 15:31:56 +0100

    create EmployeeForm component

---
+++ client/src/components/EmployeeForm/EmployeeForm.js
@@ -0,0 +1,60 @@
+import PropTypes from "prop-types";
+import { Typography } from "@material-ui/core";
+
+import { TextInputController } from "components/LogRegisterForm/TextInputController";
+import { StyledFormButton } from "components/LogRegisterForm/StyledFormButton";
+import { StyledButton } from "components/StyledButton";
+import { useNavigate } from "react-router-dom";
+
+const informationStyle = { color: "#d63e2f", letterSpacing: 2, wordSpacing: 3 };
+
+const EmployeeForm = (props) => {
+  const { control, errors, handleSubmitData, buttonText } = props;
+  const navigate = useNavigate();
+  const handleNavigate = () => navigate(-1);
+
+  return (
+    <>
+      <form style={{ maxWidth: 400, margin: "0 auto", marginTop: 50 }}>
+        {/* <FormHeader avatar={<HowToRegOutlinedIcon />} heading="registration" /> */}
+
+        <Typography
+          color="textSecondary"
+          variant="body2"
+          style={informationStyle}
+        >
+          *Fields required
+        </Typography>
+
+        <TextInputController
+          control={control}
+          name="name"
+          label="name"
+          defaultValue=""
+          error={!!errors.surname}
+          message={errors.name?.message ?? ""}
+        />
+
+        <TextInputController
+          control={control}
+          name="surname"
+          label="surname"
+          defaultValue=""
+          error={!!errors.surname}
+          message={errors.surname?.message ?? ""}
+        />
+
+        <StyledFormButton onClick={handleSubmitData} text={buttonText} />
+      </form>
+      <StyledButton onClick={handleNavigate} />
+    </>
+  );
+};
+
+EmployeeForm.propTypes = {
+  control: PropTypes.object,
+  errors: PropTypes.object,
+  handleSubmitData: PropTypes.func,
+};
+
+export default EmployeeForm;
