b229cddedba7b97e5b15e9b42819b2fe4b48ada3
TelamonPegasus <telamon025115@gmail.com>
2022-07-08 20:57:47 +0100

    add functionality with roles

--- client/src/components/EmployeesTable/EmployeesTable.js
+++ client/src/components/EmployeesTable/EmployeesTable.js
@@ -10,15 +10,30 @@ import Button from "@mui/material/Button";
 import DeleteIcon from "@mui/icons-material/Delete";
 import ModeEditIcon from "@mui/icons-material/ModeEdit";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import { useAuthContext } from "context/AuthProvider";
+
+const toastConfig = {
+  position: "top-center",
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+};
 
 const styles = {
   headingTable: { fontSize: 20 },
+  tableContainer: { maxWidth: 600, marginBottom: 30 },
 };
-const EmployeesTable = ({ employeesData, onRemove }) => (
-  <div>
+
+const EmployeesTable = ({ employeesData, onRemove, allowedRoles }) => (
+  <>
     <p style={styles.headingTable}>List:</p>
 
-    <TableContainer component={Paper} style={{ maxWidth: 600 }}>
+    <TableContainer component={Paper} style={styles.tableContainer}>
       <Table aria-label="simple table">
         <TableHead>
           <TableRow>
@@ -37,18 +52,20 @@ const EmployeesTable = ({ employeesData, onRemove }) => (
               employee={employee}
               onRemove={onRemove}
               index={index}
+              allowedRoles={allowedRoles}
             />
           ))}
         </TableBody>
       </Table>
     </TableContainer>
-  </div>
+  </>
 );
 
-const Employee = ({ employee, onRemove, index }) => {
+const Employee = ({ employee, onRemove, index, allowedRoles }) => {
   const { _id: id, name, surname, date } = employee;
 
   const navigate = useNavigate();
+  const { auth } = useAuthContext();
 
   return (
     <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
@@ -62,14 +79,29 @@ const Employee = ({ employee, onRemove, index }) => {
         <Button
           variant="outlined"
           startIcon={<ModeEditIcon />}
-          onClick={() => navigate(`/update-employee/${id}`)}
+          onClick={() =>
+            auth?.roles.filter((role) => allowedRoles.includes(role)).length
+              ? navigate(`/update-employee/${id}`)
+              : toast.error(
+                  () =>
+                    "You can not update the data. Only admin or editor can do it!",
+                  toastConfig
+                )
+          }
         />
       </TableCell>
       <TableCell>
         <Button
           variant="outlined"
           startIcon={<DeleteIcon />}
-          onClick={() => onRemove(id)}
+          onClick={() =>
+            auth?.roles.find((role) => role === 5150)
+              ? onRemove(id)
+              : toast.error(
+                  () => "You can not remove the data. Only admin can do it!",
+                  toastConfig
+                )
+          }
         />
       </TableCell>
     </TableRow>
