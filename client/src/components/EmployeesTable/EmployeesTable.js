5556d4f07215f674b0e13f217f3d4ec7201d58f9
TelamonPegasus <telamon025115@gmail.com>
2022-07-09 11:02:23 +0100

    add button

--- client/src/components/EmployeesTable/EmployeesTable.js
+++ client/src/components/EmployeesTable/EmployeesTable.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { makeStyles } from "@material-ui/core";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -9,6 +9,7 @@ import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import DeleteIcon from "@mui/icons-material/Delete";
 import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import { AiOutlineUserAdd } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
@@ -27,39 +28,70 @@ const toastConfig = {
 const styles = {
   headingTable: { fontSize: 20 },
   tableContainer: { maxWidth: 600, marginBottom: 30 },
+  linkIcon: { paddingRight: 3, fontSize: 20, color: "#d63e2f" },
 };
 
-const EmployeesTable = ({ employeesData, onRemove, allowedRoles }) => (
-  <>
-    <p style={styles.headingTable}>List:</p>
+const useStyles = makeStyles((theme) => ({
+  button: {
+    color: "#d63e2f",
+  },
+}));
 
-    <TableContainer component={Paper} style={styles.tableContainer}>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>No</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Surname</TableCell>
-            <TableCell>Date</TableCell>
-            <TableCell>Update</TableCell>
-            <TableCell>Remove</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {employeesData.map((employee, index) => (
-            <Employee
-              key={employee._id}
-              employee={employee}
-              onRemove={onRemove}
-              index={index}
-              allowedRoles={allowedRoles}
-            />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  </>
-);
+const EmployeesTable = ({ employeesData, onRemove, allowedRoles }) => {
+  const classes = useStyles();
+  const navigate = useNavigate();
+  const { auth } = useAuthContext();
+
+  const handleOnClick = () => {
+    auth?.roles.find((role) => role === 5150)
+      ? navigate("/create-employee")
+      : toast.error(
+          () => "You can not add a new Employee. Only admin can do it!",
+          toastConfig
+        );
+  };
+
+  return (
+    <>
+      <p style={styles.headingTable}>List:</p>
+
+      <TableContainer component={Paper} style={styles.tableContainer}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>No</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Surname</TableCell>
+              <TableCell>Date</TableCell>
+              <TableCell>Update</TableCell>
+              <TableCell>Remove</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {employeesData.map((employee, index) => (
+              <Employee
+                key={employee._id}
+                employee={employee}
+                onRemove={onRemove}
+                index={index}
+                allowedRoles={allowedRoles}
+              />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Button
+        variant="outlined"
+        className={classes.button}
+        onClick={handleOnClick}
+      >
+        <AiOutlineUserAdd style={styles.linkIcon} />
+        add employee
+      </Button>
+    </>
+  );
+};
 
 const Employee = ({ employee, onRemove, index, allowedRoles }) => {
   const { _id: id, name, surname, date } = employee;
