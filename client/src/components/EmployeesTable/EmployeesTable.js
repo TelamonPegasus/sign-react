547d4ba2c401d553af0c939259b5bb1c6064be23
TelamonPegasus <telamon025115@gmail.com>
2022-07-17 13:46:37 +0100

    modify imports, add styles, remove Button and ome functionality

--- client/src/components/EmployeesTable/EmployeesTable.js
+++ client/src/components/EmployeesTable/EmployeesTable.js
@@ -1,15 +1,15 @@
-import { makeStyles } from "@material-ui/core";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import Button from "@mui/material/Button";
+import {
+  Paper,
+  Button,
+  Table,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import ModeEditIcon from "@mui/icons-material/ModeEdit";
-import { AiOutlineUserAdd } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
@@ -26,72 +26,37 @@ const toastConfig = {
 };
 
 const styles = {
-  headingTable: { fontSize: 20 },
-  tableContainer: { maxWidth: 600, marginBottom: 30 },
+  tableContainer: { maxWidth: 700, marginBottom: 30 },
   linkIcon: { paddingRight: 3, fontSize: 20, color: "#d63e2f" },
 };
 
-const useStyles = makeStyles((theme) => ({
-  button: {
-    color: "#d63e2f",
-  },
-}));
-
-const EmployeesTable = ({ employeesData, onRemove, allowedRoles }) => {
-  const classes = useStyles();
-  const navigate = useNavigate();
-  const { auth } = useAuthContext();
-
-  const handleOnClick = () => {
-    auth?.roles.find((role) => role === 5150)
-      ? navigate("/create-employee")
-      : toast.error(
-          () => "You can not add a new Employee. Only admin can do it!",
-          toastConfig
-        );
-  };
-
-  return (
-    <>
-      <p style={styles.headingTable}>List:</p>
-
-      <TableContainer component={Paper} style={styles.tableContainer}>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>No</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Surname</TableCell>
-              <TableCell>Date</TableCell>
-              <TableCell>Update</TableCell>
-              <TableCell>Remove</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {employeesData.map((employee, index) => (
-              <Employee
-                key={employee._id}
-                employee={employee}
-                onRemove={onRemove}
-                index={index}
-                allowedRoles={allowedRoles}
-              />
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-
-      <Button
-        variant="outlined"
-        className={classes.button}
-        onClick={handleOnClick}
-      >
-        <AiOutlineUserAdd style={styles.linkIcon} />
-        add employee
-      </Button>
-    </>
-  );
-};
+const EmployeesTable = ({ employeesData, onRemove, allowedRoles }) => (
+  <TableContainer component={Paper} style={styles.tableContainer}>
+    <Table aria-label="simple table">
+      <TableHead>
+        <TableRow>
+          <TableCell>No</TableCell>
+          <TableCell>Name</TableCell>
+          <TableCell>Surname</TableCell>
+          <TableCell>Date</TableCell>
+          <TableCell>Update</TableCell>
+          <TableCell>Remove</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {employeesData.map((employee, index) => (
+          <Employee
+            key={employee._id}
+            employee={employee}
+            onRemove={onRemove}
+            index={index}
+            allowedRoles={allowedRoles}
+          />
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+);
 
 const Employee = ({ employee, onRemove, index, allowedRoles }) => {
   const { _id: id, name, surname, date } = employee;
