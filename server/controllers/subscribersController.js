c684622cc6a99cd8a36c99a805b304223442e14f
TelamonPegasus <telamon025115@gmail.com>
2022-07-17 13:58:30 +0100

    create subscribersControllers

---
+++ server/controllers/subscribersController.js
@@ -0,0 +1,74 @@
+const Subscriber = require("../model/Subscriber");
+
+const getAllSubscribers = async (req, res) => {
+  try {
+    const response = await Subscriber.find();
+
+    return res.status(200).send(response);
+  } catch (err) {
+    res.status(500).send({ error: "server error" });
+  }
+};
+
+const getOneSubscriber = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const response = await Subscriber.findById(id);
+
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+const updateSubscriber = async (req, res) => {
+  const id = req.params.id;
+
+  const Admin = { Admin: 5150 };
+  const Editor = { Editor: 1984 };
+  const User = { User: 2001 };
+
+  const roles = req.body.roles;
+
+  function findRole() {
+    if (roles === "5150") {
+      return Admin;
+    } else if (roles === "1984") {
+      return Editor;
+    } else {
+      return User;
+    }
+  }
+
+  const updatedData = {
+    roles: findRole(),
+  };
+
+  try {
+    const response = await Subscriber.findByIdAndUpdate(id, updatedData);
+
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+const deleteOneSubscriber = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const response = await Subscriber.findByIdAndDelete(id);
+
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+module.exports = {
+  getAllSubscribers,
+  getOneSubscriber,
+  updateSubscriber,
+  deleteOneSubscriber,
+};
