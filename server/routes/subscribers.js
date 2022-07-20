3fb5eb66013d80ed76f4fa66212b95d17e2743ba
TelamonPegasus <telamon025115@gmail.com>
2022-07-20 19:22:30 +0100

    add verifyRoles and roles to the routes

--- server/routes/subscribers.js
+++ server/routes/subscribers.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const subscribersController = require("../controllers/subscribersController");
+const ROLES_LIST = require("../config/rolesList");
+const verifyRoles = require("../middleware/verifyRoles");
 
 router.route("/").get(subscribersController.getAllSubscribers);
 
 router
   .route("/:id")
   .get(subscribersController.getOneSubscriber)
-  .put(subscribersController.updateSubscriber)
-  .delete(subscribersController.deleteOneSubscriber);
+  .put(
+    verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),
+    subscribersController.updateSubscriber
+  )
+  .delete(
+    verifyRoles(ROLES_LIST.Admin),
+    subscribersController.deleteOneSubscriber
+  );
 
 module.exports = router;
