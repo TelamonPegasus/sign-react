f944a7acb3a96d8be29cc74a99267c74fa02eebc
TelamonPegasus <telamon025115@gmail.com>
2022-07-20 19:17:20 +0100

    clean code

--- server/middleware/verifyRoles.js
+++ server/middleware/verifyRoles.js
@@ -1,6 +1,5 @@
 const verifyRoles = (...allowedRoles) => {
   return (req, res, next) => {
-    console.log(req);
     if (!req.body?.roles) {
       return res.sendStatus(401);
     }
@@ -8,10 +7,8 @@ const verifyRoles = (...allowedRoles) => {
     const rolesArray = [...allowedRoles];
 
     const result = req.body?.roles
-      .map((role) => {
-        return rolesArray.includes(role);
-      })
-      .find((val) => val === true);
+      .map((role) => rolesArray.includes(role))
+      .find((val) => val);
 
     if (!result) {
       return res.sendStatus(401);
