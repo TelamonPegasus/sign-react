d8293e6a8bb27bbdc383b10ff36919672b0ce147
TelamonPegasus <telamon025115@gmail.com>
2022-07-23 21:30:36 +0100

    remove if statement, use short words req and res

--- server/controllers/registerController.js
+++ server/controllers/registerController.js
@@ -1,33 +1,26 @@
 const Subscriber = require("../model/Subscriber");
 
-const handleRegisterNewSubscriber = async (request, response) => {
-  const { name, email, password } = request.body;
+const handleRegisterNewSubscriber = async (req, res) => {
+  const { email } = req.body;
 
-  if (!name || !email || !password) {
-    return response.status(400).json({ message: "All fields are required" });
-  }
-
-  // check for duplicate Subscribernames in the db
   const isSubscriberExist = await Subscriber.findOne({ email }).exec();
 
   if (isSubscriberExist) {
-    return response
+    return res
       .status(409)
-      .json({ message: "This email already exist. Please log in!" }); // Subscriber already exist!
+      .json({ message: "This email already exist. Please log in" }); // Subscriber already exist!
   }
 
   try {
     const subscriber = new Subscriber({
-      ...request.body,
+      ...req.body,
       roles: { User: 2001 },
     });
     await subscriber.save();
 
-    response
-      .status(201)
-      .json({ message: "You are now registered. Please log in!" });
+    res.status(201).json({ message: "You are now registered. Please log in" });
   } catch (error) {
-    response.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
 
