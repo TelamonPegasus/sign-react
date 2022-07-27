fdeb1d8aacf030924a53de41640dde7860a7abb7
TelamonPegasus <telamon025115@gmail.com>
2022-07-27 13:14:08 +0100

    add verifyJWT to the methods

--- server/routes/subscribers.js
+++ server/routes/subscribers.js
@@ -3,17 +3,20 @@ const router = express.Router();
 const subscribersController = require("../controllers/subscribersController");
 const ROLES_LIST = require("../config/rolesList");
 const verifyRoles = require("../middleware/verifyRoles");
+const verifyJWT = require("../middleware/verifyJWT");
 
-router.route("/").get(subscribersController.getAllSubscribers);
+router.route("/").get(verifyJWT, subscribersController.getAllSubscribers);
 
 router
   .route("/:id")
-  .get(subscribersController.getOneSubscriber)
+  .get(verifyJWT, subscribersController.getOneSubscriber)
   .put(
+    verifyJWT,
     verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),
     subscribersController.updateSubscriber
   )
   .delete(
+    verifyJWT,
     verifyRoles(ROLES_LIST.Admin),
     subscribersController.deleteOneSubscriber
   );
